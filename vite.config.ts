941fadf237ca8784c6ce1c2457e8ebd963a06c15
ShadowShark99 <kadenwchan@gmail.com>
2025-07-18 12:20:14 -0500

    setup testing environtment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js'
+  }
+})
+
+// vitest config
+import { defineConfig as defineVitestConfig } from 'vitest/config'
+
+export const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js'
+  },
 })
