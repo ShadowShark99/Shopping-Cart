2b2945c904bc89a78125e94eddd57bbeb3573489
ShadowShark99 <kadenwchan@gmail.com>
2025-07-21 13:33:12 -0500

    connect to FakeSHopAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navigation from './components/Navigation'
 import { Outlet } from 'react-router-dom'
 
 function App() {
+  
+  //fetch('https://fakestoreapi.com/products')
+  // .then(response => response.json())
+  // .then(data => console.log(data));
 
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      //const response = await fetch('https://fakestoreapi.com/products');
+      const response = await fetch('https://fakestoreapi.com/products');
+      const result = await response.json();
+      console.log(result);
+      setProducts(result);
+    };
+    fetchProducts();
+  }, []);
   return (
     <div className ="layout">
       <Navigation></Navigation>
-      <Outlet></Outlet>
+      <Outlet context={products}></Outlet>
     </div>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useOutletContext } from 'react-router-dom'
 
 const Home = () => {
+
+  const products = useOutletContext();
+  console.log(products);
+
   return (
-    <h2>Welcome Home</h2>
+    <div className="home-page">
+      <h2>Welcome Home To SharkBite</h2>
+      <div className="advertise">
+        <div className="products">
+          <img></img>
+          <img></img>
+          <img></img>
+        </div>
+        <div>
+          Buy things for your pet shark!
+        </div>
+      </div>
+    </div>
   )
 }
 
