5f847da76c54b906dd6d9ecc16c8d3b1287ff9f5
ShadowShark99 <kadenwchan@gmail.com>
2025-07-23 14:56:39 -0500

    Configure for build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
 import routes from './routes.tsx';
 
 const router = createBrowserRouter(routes);
